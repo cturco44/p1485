1f39084363badf58496dd1db934a8414ee816cc1
Connor Turco <connorturco@Connors-MacBook-Pro.local>
2020-09-13 13:16:07 -0400

    click parsing working

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -1,10 +1,21 @@
 """Build static HTML site from directory of HTML templates and plain files."""
+import click
 
+@click.command()
+@click.argument('INPUT_DIR')
+@click.option('--output', '-o', default="", help='Output directory.',)
+@click.option('--verbose', '-v', is_flag=True, help='Print more output.',)
 
-def main():
-    """Top level command line interface."""
-    print("Hello world!")
+def main(input_dir, output, verbose):
+    """Templated static website generator."""
+    if output == "":
+        output = input_dir
+    print(input_dir)
+    print(output)
+    print(verbose)
+
 
 
 if __name__ == "__main__":
     main()
+    
