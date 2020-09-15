00928161daa0c6bd4ecac8dce7c88f7289616e72
Connor Turco <connorturco@Connors-MacBook-Pro.local>
2020-09-15 16:20:27 -0400

    before messing with style

--- insta485generator/__main__.py
+++ insta485generator/__main__.py
@@ -6,17 +6,17 @@ import sys
 import click
 import jinja2
 
+
 @click.command()
 @click.argument('INPUT_DIR')
 @click.option('--output', '-o', default="", help='Output directory.',)
 @click.option('--verbose', '-v', is_flag=True, help='Print more output.',)
-
 def main(input_dir, output, verbose):
     """Templated static website generator."""
-    #Don't forget to use verbose and input path
+    # Don't forget to use verbose and input path
     json_path = pathlib.Path(input_dir) / "config.json"
 
-    #Open JSON File
+    # Open JSON File
     with open(json_path, mode='r') as file:
         try:
             data = json.load(file)
@@ -24,11 +24,12 @@ def main(input_dir, output, verbose):
             print("JSON ERROR")
             sys.exit(1)
     for item in data:
-        #Determine output path
+        # Determine output path
         url = item["url"].lstrip("/")  # remove leading slash
         input_dir = pathlib.Path(input_dir)  # convert str to Path object
         template_dir = input_dir / "templates"
-        output_dir = input_dir/"html"  # default, can be changed with --output option
+        # default, can be changed with --output option
+        output_dir = input_dir/"html"
         if output != "":
             output_dir = pathlib.Path(output)
         output_path = output_dir/url
@@ -47,25 +48,17 @@ def main(input_dir, output, verbose):
         if (input_dir/"static").exists():
             copyer(str(input_dir/"static"), str(output_dir))
             if verbose:
-                print("Copied " + str(input_dir/"static") + " -> " + str(output_path))
-        env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(template_dir)),
-                                 autoescape=jinja2.select_autoescape(['html', 'xml']),)
+                print("Copied " + str(input_dir/"static")
+                      + " -> " + str(output_path))
+        env = \
+                jinja2.Environment(loader=jinja2.FileSystemLoader(str(template_dir)),
+                                   autoescape=jinja2.select_autoescape(['html',
+                                   'xml']))
         template = env.get_template(item["template"])
         output_file.write_text(template.render(item['context']))
 
         if verbose:
             print("Rendered " + item["template"] + " -> " + str(output_file))
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
